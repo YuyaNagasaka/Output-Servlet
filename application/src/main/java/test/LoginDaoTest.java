dcc26586d70f6f145d5c96da8c062bf4cc78d27d
Yuya Nagasaka <nagasaka.yuya0928@gmail.com>
2021-11-13 20:36:28 +0900

    [ADD]LoginDAOTest LoginDAOでDB接続がされるかのテスト

---
+++ application/src/main/java/test/LoginDaoTest.java
@@ -0,0 +1,9 @@
+package test;
+
+import dao.LoginDAO;
+public class LoginDaoTest {
+  public static void main(String[] args) {
+	  LoginDAO login_dao= new LoginDAO();
+	  login_dao.Login();
+  }  
+}
