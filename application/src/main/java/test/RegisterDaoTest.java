ad849ec4451f920f92e637b2a7677dd6d36a33b2
Yuya Nagasaka <nagasaka.yuya0928@gmail.com>
2021-11-13 23:50:30 +0900

    [ADD]RegisterDaoTest DB接続テスト

---
+++ application/src/main/java/test/RegisterDaoTest.java
@@ -0,0 +1,10 @@
+package test;
+
+import dao.RegisterDAO;
+
+public class RegisterDaoTest {
+	public static void main(String[] args) {
+		  RegisterDAO register_dao= new RegisterDAO();
+		  register_dao.Register();
+	  }  
+}
