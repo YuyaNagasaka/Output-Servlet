51c09e19c9fe5dcd41ba4d120e3bac1b957c736a
Yuya Nagasaka <nagasaka.yuya0928@gmail.com>
2021-11-14 18:58:35 +0900

    [ADD]UpdateDAO DB接続設定及び投稿更新機能の実装

---
+++ application/src/main/java/dao/UpdateDAO.java
@@ -0,0 +1,53 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import model.Output;
+
+public class UpdateDAO {
+	  String url = "jdbc:mysql://localhost/application";
+	  String user = "root";
+	  String password = "Nagasaka0928";
+	    
+	  public void Update(Output info) {
+	    Connection conn = null;
+		  try {
+		    Class.forName("com.mysql.jdbc.Driver");//使用するJDBCドライバの指定
+		    conn = DriverManager.getConnection(url,user,password);//データベース接続
+		    System.out.println("DBとの接続成功");
+		    String sql = "UPDATE output SET title = ?,description = ?,keyword = ? WHERE id = ?"; 
+		    PreparedStatement statement = conn.prepareStatement(sql);//SQLの送信元にSQL文を渡す
+		    //未確定のパラメータに値を代入
+		    statement.setString(1,info.getTitle());
+		    statement.setString(2,info.getDescription());
+		    statement.setString(3,info.getKeyword());
+		    statement.setInt(4,info.getId());
+		    int result = statement.executeUpdate();//SQL文の実行 追加したデータの行数が戻り値になる
+		      
+		      if(result != 0) {
+		        System.out.println("更新成功");
+		      }else {
+		   	    System.out.println("更新失敗");
+		      }
+		    }
+		    
+		    catch(SQLException e) {
+		      e.printStackTrace();
+		    }
+		    catch(ClassNotFoundException e) {
+		      e.printStackTrace();
+		    }
+		    finally {
+		      if (conn != null){
+		        try {
+		    	  conn.close();
+		        }catch(SQLException e) {
+		    	  e.printStackTrace();
+		        }
+		    }
+	      }
+	   }
+	}
