bbdbbb67e6f4a675dde8d4cbe6c4596a21ce5e86
Yuya Nagasaka <nagasaka.yuya0928@gmail.com>
2021-11-14 16:32:24 +0900

    [ADD]LoginDAO ログイン機能を追加

--- application/src/main/java/dao/LoginDAO.java
+++ application/src/main/java/dao/LoginDAO.java
@@ -2,20 +2,33 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import model.User;
+
 public class LoginDAO {
-  public void Login() {
+  public void Login(User info) {
     Connection conn = null; //DBへの接続や切断を行う
     String url = "jdbc:mysql://localhost/application";
     String user = "root";
     String password = "Nagasaka0928";
     try {
       Class.forName("com.mysql.jdbc.Driver");//使用するJDBCドライバの指定
-      conn = DriverManager.getConnection
-      (url,user,password);//データベース接続
+      conn = DriverManager.getConnection(url,user,password);//データベース接続
       System.out.println("DBとの接続成功");
-     }
+      String sql = "SELECT * FROM application.user where name=? and password = ?";
+      PreparedStatement statement = conn.prepareStatement(sql);
+      statement.setString(1,info.getName());
+      statement.setString(2,info.getPassword());
+      ResultSet result = statement.executeQuery();
+      if(result.next()) {
+    	  System.out.println("ログインに成功しました");
+      }else {
+    	  System.out.println("ログインに失敗しました");
+      }
+    }
     
     catch(SQLException e) {
     	e.printStackTrace();
@@ -31,7 +44,6 @@ public class LoginDAO {
     		e.printStackTrace();
           }
     	}
-    	
     }
   }
 }
