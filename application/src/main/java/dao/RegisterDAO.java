7bd97237941806ccd121ddcada949469553d7e09
Yuya Nagasaka <nagasaka.yuya0928@gmail.com>
2021-11-14 14:18:16 +0900

    [ADD]RegisterDAO 会員登録処理の追加

--- application/src/main/java/dao/RegisterDAO.java
+++ application/src/main/java/dao/RegisterDAO.java
@@ -2,35 +2,53 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+import model.User;
+
 public class RegisterDAO {
-	public void Register() {
-	    Connection conn = null; //DBへの接続や切断を行う
-	    String url = "jdbc:mysql://localhost/application";
-	    String user = "root";
-	    String password = "Nagasaka0928";
-	    try {
-	      Class.forName("com.mysql.jdbc.Driver");//使用するJDBCドライバの指定
-	      conn = DriverManager.getConnection
-	      (url,user,password);//データベース接続
-	      System.out.println("DBとの接続成功");
-	     }
+	
+  String url = "jdbc:mysql://localhost/application";
+  String user = "root";
+  String password = "Nagasaka0928";
+    
+  public void Register(User info) {
+    Connection conn = null;
+	  try {
+	    Class.forName("com.mysql.jdbc.Driver");//使用するJDBCドライバの指定
+	    conn = DriverManager.getConnection(url,user,password);//データベース接続
+	    System.out.println("DBとの接続成功");
+	    String sql = "INSERT INTO user (id,name,password,output_times) values(?,?,?,?)"; 
+	    PreparedStatement statement = conn.prepareStatement(sql);//SQLの送信元にSQL文を渡す
+	    //未確定のパラメータに値を代入
+	    statement.setInt(1,info.getId());
+	    statement.setString(2,info.getName());
+	    statement.setString(3, info.getPassword());
+	    statement.setInt(4,info.getOutputTimes());
+	    int result = statement.executeUpdate();//SQL文の実行 追加したデータの行数が戻り値になる
+	      
+	      if(result != 0) {
+	        System.out.println("新規登録成功");
+	      }else {
+	   	    System.out.println("新規登録失敗");
+	      }
+	    }
 	    
 	    catch(SQLException e) {
-	    	e.printStackTrace();
+	      e.printStackTrace();
 	    }
 	    catch(ClassNotFoundException e) {
-	    	e.printStackTrace();
+	      e.printStackTrace();
 	    }
 	    finally {
-	    	if (conn != null){
-	          try {
+	      if (conn != null){
+	        try {
 	    	  conn.close();
-	          }catch(SQLException e) {
-	    		e.printStackTrace();
-	          }
-	    	}
+	        }catch(SQLException e) {
+	    	  e.printStackTrace();
+	        }
 	    }
-	  }
+      }
+   }
 }
