0116dcecba6b5c9334372a1038c94d366b29fab1
Yuya Nagasaka <nagasaka.yuya0928@gmail.com>
2021-11-14 16:33:01 +0900

    [ADD]LoginDaoTest ログイン機能のテストを実装

--- application/src/main/java/test/LoginDaoTest.java
+++ application/src/main/java/test/LoginDaoTest.java
@@ -1,9 +1,21 @@
 package test;
 
 import dao.LoginDAO;
+import model.User;
+
 public class LoginDaoTest {
   public static void main(String[] args) {
 	  LoginDAO login_dao= new LoginDAO();
-	  login_dao.Login();
+	  User user = new User();
+	  String name = "Test";
+	  String pass = "Test";
+	  user.setName(name);
+	  user.setPassword(pass);
+	  login_dao.Login(user);
+	  name = "dummy";
+	  pass = "dummy";
+	  user.setName(name);
+	  user.setPassword(pass);
+	  login_dao.Login(user);
   }  
 }
