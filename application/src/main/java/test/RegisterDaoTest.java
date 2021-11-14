7f6d6f044c6f280523264793bf1a86d27f25a44f
Yuya Nagasaka <nagasaka.yuya0928@gmail.com>
2021-11-14 14:31:46 +0900

    [ADD]RegisterDaoTest 会員登録テスト

--- application/src/main/java/test/RegisterDaoTest.java
+++ application/src/main/java/test/RegisterDaoTest.java
@@ -1,10 +1,20 @@
 package test;
 
 import dao.RegisterDAO;
+import model.User;
 
 public class RegisterDaoTest {
 	public static void main(String[] args) {
+		  User user = new User();
+		  int test = 2;
+		  String name = "Test";
+		  String pass = "Test";
+		  int quantitiy = 1;
+		  user.setId(test);
+		  user.setName(name);
+		  user.setPassword(pass);
+		  user.setOutputTimes(quantitiy);
 		  RegisterDAO register_dao= new RegisterDAO();
-		  register_dao.Register();
+		  register_dao.Register(user);
 	  }  
 }
