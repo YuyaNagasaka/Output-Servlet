fd744fc4206804b75a711d068588c0a0bcb00a87
Yuya Nagasaka <nagasaka.yuya0928@gmail.com>
2021-11-16 16:58:45 +0900

    [FIX] DB操作の修正

--- application/src/main/java/dao/LoginDAO.java
+++ application/src/main/java/dao/LoginDAO.java
@@ -9,32 +9,42 @@ import java.sql.SQLException;
 import model.User;
 
 public class LoginDAO {
-  public void Login(User info) {
-    Connection conn = null; //DBへの接続や切断を行う
-    String url = "jdbc:mysql://localhost/application";
-    String user = "root";
-    String password = "Nagasaka0928";
+  
+  User user_info = new User();
+  Connection conn = null; //DBへの接続や切断を行う	
+  
+  String url = "jdbc:mysql://localhost/application";
+  String user = "root";
+  String password = "Nagasaka0928";
+
+  public User Login(User info) {
     try {
       Class.forName("com.mysql.jdbc.Driver");//使用するJDBCドライバの指定
       conn = DriverManager.getConnection(url,user,password);//データベース接続
-      System.out.println("DBとの接続成功");
-      String sql = "SELECT * FROM application.user where name=? and password = ?";
+      
+      String sql = "SELECT * FROM user where name=? and password = ?";
       PreparedStatement statement = conn.prepareStatement(sql);
       statement.setString(1,info.getName());
       statement.setString(2,info.getPassword());
       ResultSet result = statement.executeQuery();
-      if(result.next()) {
-    	  System.out.println("ログインに成功しました");
+      
+      if (result.next()){
+      user_info.setName(result.getString("name"));
+      user_info.setPassword(result.getString("password"));
+      user_info.setId(result.getInt("id"));
+      return user_info;
       }else {
-    	  System.out.println("ログインに失敗しました");
+    	  return null;
       }
     }
     
     catch(SQLException e) {
     	e.printStackTrace();
+    	return null;
     }
     catch(ClassNotFoundException e) {
     	e.printStackTrace();
+    	return null;
     }
     finally {
     	if (conn != null){
--- application/src/main/java/dao/RegisterDAO.java
+++ application/src/main/java/dao/RegisterDAO.java
@@ -3,41 +3,54 @@ package dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import model.User;
 
 public class RegisterDAO {
-	
+  
+  User user_info = new User();
   String url = "jdbc:mysql://localhost/application";
   String user = "root";
-  String password = "Nagasaka0928";
-    
-  public void Register(User info) {
+  String password = "Nagasaka0928"; 
+ 
+  public User Register(User info) {
     Connection conn = null;
 	  try {
 	    Class.forName("com.mysql.jdbc.Driver");//使用するJDBCドライバの指定
 	    conn = DriverManager.getConnection(url,user,password);//データベース接続
 	    System.out.println("DBとの接続成功");
+	    
 	    String sql = "INSERT INTO user (name,password) values(?,?)"; 
 	    PreparedStatement statement = conn.prepareStatement(sql);//SQLの送信元にSQL文を渡す
 	    //未確定のパラメータに値を代入
 	    statement.setString(1,info.getName());
-	    statement.setString(2, info.getPassword());
-	    int result = statement.executeUpdate();//SQL文の実行 追加したデータの行数が戻り値になる
-	      
-	      if(result != 0) {
-	        System.out.println("新規登録成功");
-	      }else {
-	   	    System.out.println("新規登録失敗");
-	      }
-	    }
+	    statement.setString(2,info.getPassword());
+	    statement.executeUpdate();//SQL文の実行 追加したデータの行数が戻り値になる
 	    
+	    String sql2 = "SELECT * FROM user where name=? and password = ?";
+	    
+	    statement = conn.prepareStatement(sql2);
+	    statement.setString(1,info.getName());
+	    statement.setString(2,info.getPassword());
+	    ResultSet result = statement.executeQuery();
+	      
+	    if (result.next()){
+	      user_info.setName(result.getString("name"));
+	      user_info.setPassword(result.getString("password"));
+	      user_info.setId(result.getInt("id"));
+	      return user_info;
+	      }else {
+	    	  return null;
+	    }}
 	    catch(SQLException e) {
 	      e.printStackTrace();
+	      return null;
 	    }
 	    catch(ClassNotFoundException e) {
 	      e.printStackTrace();
+	      return null;
 	    }
 	    finally {
 	      if (conn != null){
@@ -46,7 +59,7 @@ public class RegisterDAO {
 	        }catch(SQLException e) {
 	    	  e.printStackTrace();
 	        }
-	    }
+	    } 
       }
    }
 }
