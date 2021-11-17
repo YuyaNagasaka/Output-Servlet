8e6ff955b45bc629b6e15ce326511bd425942060
Yuya Nagasaka <nagasaka.yuya0928@gmail.com>
2021-11-17 10:22:18 +0900

    [ADD]IndexServlet 投稿一覧機能作成

--- application/src/main/java/servlet/IndexServlet.java
+++ application/src/main/java/servlet/IndexServlet.java
@@ -1,13 +1,19 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import dao.IndexDAO;
+import model.Output;
+import model.User;
 @WebServlet("/IndexServlet")
 public class IndexServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -17,11 +23,17 @@ public class IndexServlet extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	  HttpSession session = request.getSession();
+	  User user = (User)session.getAttribute("user");
+	  IndexDAO index = new IndexDAO();
+	  List<Output> outputList = index.Index(user);
+	  request.setAttribute("list", outputList);
+	  RequestDispatcher dispatcher = request.getRequestDispatcher("/Index.jsp");
+	  dispatcher.forward(request, response);
+
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
 	}
 
 }
