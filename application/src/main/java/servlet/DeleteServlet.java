c86c36e6f1e33a8839705febf33064889d47e314
Yuya Nagasaka <nagasaka.yuya0928@gmail.com>
2021-11-17 10:20:08 +0900

    [ADD]DeleteServlet 投稿削除機能追加

--- application/src/main/java/servlet/DeleteServlet.java
+++ application/src/main/java/servlet/DeleteServlet.java
@@ -2,11 +2,15 @@ package servlet;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import dao.DeleteDAO;
+import model.Output;
 
 @WebServlet("/DeleteServlet")
 public class DeleteServlet extends HttpServlet {
@@ -17,7 +21,15 @@ public class DeleteServlet extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	  request.setCharacterEncoding("UTF-8");
+	  int id = Integer.parseInt(request.getParameter("id"));
+	  Output output = new Output();
+	  output.setId(id);
+	  DeleteDAO delete = new DeleteDAO();
+	  delete.Delete(output);
+	  request.setAttribute("deleteSuccessMessage","投稿が削除されました");
+	  RequestDispatcher dispatchr = request.getRequestDispatcher("/IndexServlet");
+	  dispatchr.forward(request,response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
