7c3d65de2ac17782bcc9c108143d0805b926e5f7
Yuya Nagasaka <nagasaka.yuya0928@gmail.com>
2021-11-17 10:20:56 +0900

    [FIX]DeleteDAO リファクタリング

--- application/src/main/java/dao/DeleteDAO.java
+++ application/src/main/java/dao/DeleteDAO.java
@@ -18,19 +18,12 @@ public class DeleteDAO {
 		    Class.forName("com.mysql.jdbc.Driver");//使用するJDBCドライバの指定
 		    conn = DriverManager.getConnection(url,user,password);//データベース接続
 		    System.out.println("DBとの接続成功");
-		    String sql = "DELETE FROM application.output WHERE id= ?"; 
+		    String sql = "DELETE FROM output WHERE id= ?"; 
 		    PreparedStatement statement = conn.prepareStatement(sql);//SQLの送信元にSQL文を渡す
 		    //未確定のパラメータに値を代入
 		    statement.setInt(1,info.getId());
-		    int result = statement.executeUpdate();//SQL文の実行 追加したデータの行数が戻り値になる
-		      
-		      if(result != 0) {
-		        System.out.println("削除成功");
-		      }else {
-		   	    System.out.println("削除失敗");
-		      }
-		    }
-		    
+		    statement.executeUpdate();//SQL文の実行 追加したデータの行数が戻り値になる
+		  }
 		    catch(SQLException e) {
 		      e.printStackTrace();
 		    }
